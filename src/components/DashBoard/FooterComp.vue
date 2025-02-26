9defc131752932d5e6eba95c0fb8beaaae20f8c9
Jaimi Patel <jaimip.albiorix@gmail.com>
2025-02-26 11:29:07 +0530

    Formate

--- src/components/DashBoard/FooterComp.vue
+++ src/components/DashBoard/FooterComp.vue
@@ -1,18 +1,14 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <footer class="w-full min-h-80 max-h-none bg-gray-900 text-white text-center p-4 md:p-8 lg:p-10">
     <h1 class="text-4xl md:text-5xl lg:text-6xl italic font-bold py-1 md:py-3 lg:py-5">
       Food <span class="text-orange-600">Partner</span>
     </h1>
-    <p
-      class="text-gray-400 text-sm md:text-lg lg:text-xl mt-4 md:mt-8 lg:mt-10 mb-3 md:mb-4 lg:mb-5"
-    >
+    <p class="text-gray-400 text-sm md:text-lg lg:text-xl mt-4 md:mt-8 lg:mt-10 mb-3 md:mb-4 lg:mb-5">
       Eat healthy to live healthy. Live healthy to live happy.
     </p>
     <span class="text-gray-400 text-sm md:text-base">
-      &copy; 2025 All Rights Reserved By Jaimi Patel</span
-    >
+      &copy; 2025 All Rights Reserved By Jaimi Patel</span>
   </footer>
 </template>
--- src/components/DashBoard/HeaderComp.vue
+++ src/components/DashBoard/HeaderComp.vue
@@ -1,36 +1,36 @@
 <script setup>
-import { ShoppingCartIcon, Bars3Icon } from '@heroicons/vue/24/solid'
-import { RouterLink, useRouter } from 'vue-router'
-import { useAuthStore } from '@/stores/authStore'
-import { useCartStore } from '@/stores/cartStore'
-import toast from '@/plugin/toast'
-import { onMounted } from 'vue'
+  import { ShoppingCartIcon, Bars3Icon } from '@heroicons/vue/24/solid'
+  import { RouterLink, useRouter } from 'vue-router'
+  import { useAuthStore } from '@/stores/authStore'
+  import { useCartStore } from '@/stores/cartStore'
+  import toast from '@/plugin/toast'
+  import { onMounted } from 'vue'
 
-const router = useRouter()
-const authStore = useAuthStore()
-const cartStore = useCartStore()
+  const router = useRouter()
+  const authStore = useAuthStore()
+  const cartStore = useCartStore()
 
-const logout = () => {
-  authStore.logOut()
-  toast.success('User Logged Out successfully')
-  router.push('/login')
-}
+  const logout = () => {
+    authStore.logOut()
+    toast.success('User Logged Out successfully')
+    router.push('/login')
+  }
 
-const openSidebar = () => {
-  document.getElementById('sidebar').classList.add('open')
-  document.getElementById('overlay').classList.add('active')
-}
+  const openSidebar = () => {
+    document.getElementById('sidebar').classList.add('open')
+    document.getElementById('overlay').classList.add('active')
+  }
 
-const closeSidebar = () => {
-  setTimeout(() => {
-    document.getElementById('sidebar').classList.remove('open')
-    document.getElementById('overlay').classList.remove('active')
-  }, 200)
-}
+  const closeSidebar = () => {
+    setTimeout(() => {
+      document.getElementById('sidebar').classList.remove('open')
+      document.getElementById('overlay').classList.remove('active')
+    }, 200)
+  }
 
-onMounted(() => {
-  if (authStore.userData?.token) cartStore.getCartValue()
-})
+  onMounted(() => {
+    if (authStore.userData?.token) cartStore.getCartValue()
+  })
 </script>
 
 <template>
@@ -38,24 +38,17 @@ onMounted(() => {
     <div class="block sm:hidden overlay" id="overlay" @click="closeSidebar()"></div>
     <div class="flex items-center space-x-2 m-2 cursor-pointer" @click="router.push('/')">
       <img src="@/assets/img/favicon.png" alt="Logo" class="w-12 md:w-16 lg:w-20" />
-      <h1
-        class="text-2xl md:text-4xl lg:text-5xl italic font-bold py-1 md:py-3 lg:py-5 text-gray-600"
-      >
+      <h1 class="text-2xl md:text-4xl lg:text-5xl italic font-bold py-1 md:py-3 lg:py-5 text-gray-600">
         Food <span class="text-orange-600">Partner</span>
       </h1>
     </div>
 
     <div class="block sm:hidden flex items-center space-x-3 mr-5">
-      <div
-        v-if="authStore?.userData?.userId"
-        class="relative cursor-pointer hover:bg-orange-50 rounded-full"
-        @click="router.push('/cart')"
-      >
+      <div v-if="authStore?.userData?.userId" class="relative cursor-pointer hover:bg-orange-50 rounded-full"
+        @click="router.push('/cart')">
         <ShoppingCartIcon class="w-8 md:w-12 p-1.5 md:p-2" />
-        <span
-          v-if="cartStore.cart"
-          class="absolute py-0.5 md:py-1 px-1.5 md:px-2 text-xs bg-orange-500 hover:bg-orange-600 rounded-full -top-2 -right-2 text-white"
-        >
+        <span v-if="cartStore.cart"
+          class="absolute py-0.5 md:py-1 px-1.5 md:px-2 text-xs bg-orange-500 hover:bg-orange-600 rounded-full -top-2 -right-2 text-white">
           {{ cartStore.cart }}
         </span>
       </div>
@@ -66,38 +59,19 @@ onMounted(() => {
 
     <nav class="navigation-menu">
       <RouterLink to="/" class="router-link">Home</RouterLink>
-      <RouterLink v-if="authStore?.userData?.userId" to="/restaurant" class="router-link"
-        >Restaurant</RouterLink
-      >
-      <RouterLink
-        v-if="authStore?.userData?.userId && authStore?.userData?.role == 'owner'"
-        to="/combo"
-        class="router-link"
-        >Combo</RouterLink
-      >
-      <RouterLink
-        v-if="authStore?.userData?.userId && authStore?.userData?.role == 'owner'"
-        to="/item"
-        class="router-link"
-        >Item</RouterLink
-      >
-      <RouterLink
-        v-if="authStore?.userData?.userId && authStore?.userData?.role == 'owner'"
-        to="/category"
-        class="router-link"
-        >Category</RouterLink
-      >
+      <RouterLink v-if="authStore?.userData?.userId" to="/restaurant" class="router-link">Restaurant</RouterLink>
+      <RouterLink v-if="authStore?.userData?.userId && authStore?.userData?.role == 'owner'" to="/combo"
+        class="router-link">Combo</RouterLink>
+      <RouterLink v-if="authStore?.userData?.userId && authStore?.userData?.role == 'owner'" to="/item"
+        class="router-link">Item</RouterLink>
+      <RouterLink v-if="authStore?.userData?.userId && authStore?.userData?.role == 'owner'" to="/category"
+        class="router-link">Category</RouterLink>
       <span v-if="authStore?.userData?.userId" class="router-link" @click="logout()">Logout</span>
-      <div
-        v-if="authStore?.userData?.userId"
-        class="relative cursor-pointer hover:bg-orange-50 rounded-full"
-        @click="router.push('/cart')"
-      >
+      <div v-if="authStore?.userData?.userId" class="relative cursor-pointer hover:bg-orange-50 rounded-full"
+        @click="router.push('/cart')">
         <ShoppingCartIcon class="w-8 md:w-12 p-1.5 md:p-2" />
-        <span
-          v-if="cartStore.cart"
-          class="absolute py-0.5 md:py-1 px-1 md:px-2 text-xs md:text-xs bg-orange-500 hover:bg-orange-600 rounded-full -top-2 -right-2 text-white"
-        >
+        <span v-if="cartStore.cart"
+          class="absolute py-0.5 md:py-1 px-1 md:px-2 text-xs md:text-xs bg-orange-500 hover:bg-orange-600 rounded-full -top-2 -right-2 text-white">
           {{ cartStore.cart }}
         </span>
       </div>
@@ -106,59 +80,45 @@ onMounted(() => {
 
     <nav class="sidebar pt-3 shadow-xl" id="sidebar" @click="closeSidebar()">
       <RouterLink to="/" class="router-link">Home</RouterLink>
-      <RouterLink v-if="authStore?.userData?.userId" to="/restaurant" class="router-link"
-        >Restaurant</RouterLink
-      >
-      <RouterLink
-        v-if="authStore?.userData?.userId && authStore?.userData?.role == 'owner'"
-        to="/combo"
-        class="router-link"
-        >Combo</RouterLink
-      >
-      <RouterLink
-        v-if="authStore?.userData?.userId && authStore?.userData?.role == 'owner'"
-        to="/item"
-        class="router-link"
-        >Item</RouterLink
-      >
-      <RouterLink
-        v-if="authStore?.userData?.userId && authStore?.userData?.role == 'owner'"
-        to="/category"
-        class="router-link"
-        >Category</RouterLink
-      >
+      <RouterLink v-if="authStore?.userData?.userId" to="/restaurant" class="router-link">Restaurant</RouterLink>
+      <RouterLink v-if="authStore?.userData?.userId && authStore?.userData?.role == 'owner'" to="/combo"
+        class="router-link">Combo</RouterLink>
+      <RouterLink v-if="authStore?.userData?.userId && authStore?.userData?.role == 'owner'" to="/item"
+        class="router-link">Item</RouterLink>
+      <RouterLink v-if="authStore?.userData?.userId && authStore?.userData?.role == 'owner'" to="/category"
+        class="router-link">Category</RouterLink>
       <a v-if="authStore?.userData?.userId" class="router-link" @click="logout()">Logout</a>
       <RouterLink v-else to="/signup" class="router-link">Sign Up</RouterLink>
     </nav>
   </header>
 </template>
 
-<style  scoped>
-.sidebar {
-  position: fixed;
-  top: 0;
-  right: -200px;
-  width: 200px;
-  height: 100%;
-  background: #888888;
-  transition: 0.3s;
-}
+<style scoped>
+  .sidebar {
+    position: fixed;
+    top: 0;
+    right: -200px;
+    width: 200px;
+    height: 100%;
+    background: #888888;
+    transition: 0.3s;
+  }
 
-.sidebar.open {
-  right: 0;
-}
+  .sidebar.open {
+    right: 0;
+  }
 
-.overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.2);
-  display: none;
-}
+  .overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.2);
+    display: none;
+  }
 
-.overlay.active {
-  display: block;
-}
+  .overlay.active {
+    display: block;
+  }
 </style>
